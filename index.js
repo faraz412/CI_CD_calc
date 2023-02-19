7e36aaa8785b06db36cfc98155c78c1c9b20c3ad
faraz412 <ahmedfaraz412@gmail.com>
2023-02-19 18:03:25 +0530

    Index file with logic

---
+++ index.js
@@ -0,0 +1,48 @@
+// index.js
+
+//  import the crypto module
+const crypto=require('crypto');
+
+
+//  get a commands using process.argv
+let cmd=process.argv;
+const operation=cmd[2];
+const num1=Number(cmd[3]);
+const num2=Number(cmd[4]);
+
+// complete the  function
+
+
+
+switch (operation) {
+  case "add":
+    console.log(num1+num2);
+    break;
+  case "sub":
+    console.log(num1-num2);
+    break;
+  case "mult":
+    console.log(num1*num2);
+    break;
+  case "divide":
+    console.log(num1/num2);
+    break;
+  case "sin":
+    console.log(Math.sin(num1));
+    break;
+  case "cos":
+    console.log(Math.sin(cos));
+    break;
+  case "tan":
+    console.log(Math.sin(tan));
+    break;
+  case "random":
+    if(num1){
+      console.log(Math.florr(Math.random()*num1*10));
+    }else if(!num1){
+      console.log("Provide length for random number generation.")
+    }
+    break;                             
+  default:
+    console.log("Invalid operation");
+}
